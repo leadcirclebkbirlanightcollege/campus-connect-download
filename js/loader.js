2268cf0ad08c524e240dc48a9a683b22d34f76ff
leadcirclebkbirlanightcollege <leadcircle.bkbirlanightcollege@gmail.com>
2026-01-22 19:23:52 +0530

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,18 +1,19 @@
 window.addEventListener("load", () => {
   document.body.classList.add("page-loaded");
   const loader = document.getElementById("page-loader");
-  if (loader) {
-    setTimeout(() => loader.classList.add("hidden"), 300);
-  }
+  if (loader) loader.classList.add("hidden");
 });
 
+// Smooth page transition
 document.querySelectorAll("a[href]").forEach(link => {
   const url = link.getAttribute("href");
   if (url && !url.startsWith("#") && !url.startsWith("http")) {
     link.addEventListener("click", e => {
       e.preventDefault();
       document.body.classList.remove("page-loaded");
-      setTimeout(() => window.location.href = url, 300);
+      setTimeout(() => {
+        window.location.href = url;
+      }, 300);
     });
   }
 });
